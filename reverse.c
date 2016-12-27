94fce68a0b103ab76437d73c90cfe224e6879aac
brucewayne1206 <hari.6896@gmail.com>
2016-12-27 18:27:39 +0530

    Update reverse.c

--- reverse.c
+++ reverse.c
@@ -1,10 +1,9 @@
 # ZohoEnrich
-
 #include <stdio.h>
 
 int main(void) {
 	// your code goes here
-	int n,r,rev=0,mul=1;
+	int n,r,rev=0,mul=10;
 	scanf("%d",&n);
 	if(n>0)
 	{
@@ -12,7 +11,6 @@ int main(void) {
 	    {
 	        r=n%10;
 	        rev=(rev*mul)+r;
-	        mul*=10;
 	        n/=10;
 	    }while(n>0);
 	    printf("\nReverse: %d",rev);
