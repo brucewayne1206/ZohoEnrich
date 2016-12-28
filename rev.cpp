3f74dc47499bdcd178f5c18f1d03e412173a534c
brucewayne1206 <hari.6896@gmail.com>
2016-12-28 18:25:30 +0530

    Update and rename rev.c to rev.cpp

--- rev.cpp
+++ rev.cpp
@@ -6,13 +6,15 @@ using namespace std;
 int main() {
 	long num,temp,r,power;
 	int i,d;
+	cout<<"\nEnter the 5 digit number: ";
 	cin>>num;
+	cout<<"\nEnter the number of times to be rotated: ";
 	cin>>d;
 	i=d%5;
 	power=pow(10,5-i);
 	temp=num%power;
 	temp=temp*pow(10,i);
 	temp=temp+(num/power);
-	cout<<temp;
+	cout<<"\nThe reversed number: "<<temp;
 	return 0;
 }
