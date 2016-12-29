dcf98258974fcd4cdc01210c5be8ec09fd16702b
brucewayne1206 <hari.6896@gmail.com>
2016-12-29 17:31:13 +0530

    Create arrange.c

---
+++ arrange.c
@@ -0,0 +1,30 @@
+# ZohoEnrich
+#include <stdio.h>
+
+int main() 
+{
+    int size=0,num,arr[20];
+    scanf("%d",&num);
+    while(num<0)
+    {
+        a[size++]=num%10;
+        num=num/10;
+    }
+    for(int i=0;i<size;i++)
+    {
+        for(int j=i+1;j<size;j++)        {    if(arr[i]>arr[j])
+            {
+                temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+    for(int i=0;i<size;i++)
+    {
+        num=(num*10)+arr[i];
+    }
+    printf("%d",num);
+
+	return 0;
+}
