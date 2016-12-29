d4dc07c7a9e26b9705da3c44586a43de9dc27c0f
brucewayne1206 <hari.6896@gmail.com>
2016-12-29 17:39:32 +0530

    Update arrange.c

--- arrange.c
+++ arrange.c
@@ -1,10 +1,11 @@
-# ZohoEnrich
 #include <stdio.h>
 
 int main() 
 {
-    int size=0,num,arr[20];
+    int size=0,num,arr[20],temp;
+    int original;
     scanf("%d",&num);
+    original=num;
     while(num<0)
     {
         a[size++]=num%10;
@@ -13,8 +14,8 @@ int main()
     for(int i=0;i<size;i++)
     {
         for(int j=i+1;j<size;j++)        
-	{    
-	    if(arr[i]>arr[j])
+        {    
+            if(arr[i]>arr[j])
             {
                 temp=arr[i];
                 arr[i]=arr[j];
