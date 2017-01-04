32ed1364dc304cfdaa6493b4479b85526fd78f2f
brucewayne1206 <hari.6896@gmail.com>
2017-01-04 16:46:33 +0530

    Create magicsquare.cpp

---
+++ magicsquare.cpp
@@ -0,0 +1,61 @@
+# ZohoEnrich
+#include <iostream.h>
+using namespace std;
+
+int main() {
+	int n,i=0,j,cnt=1,t;
+	cin>>n;
+	j=n/2;
+	int arr[n][n];
+	while(cnt<=n*n)
+	{
+	    arr[i][j]=cnt++;
+	    if(i-1<0)
+	    {
+	        t=j;
+	        if(j+1>=n)
+	        {
+	            t=-1;
+	        }
+	        if(arr[n-1][t+1]>=1&&arr[n-1][t+1]<=n*n)
+            {
+	           i++;
+	        }
+	        else
+	        {
+	            i=n-1;
+	            j=t+1;
+	        }
+	    }
+	    else
+	    {
+	        t=j;
+	        if((t+1)==n)
+	        {
+	            t=-1;
+	        }
+	        if(arr[i-1][t+1]>=1&&arr[i-1][t+1]<=n*n)
+            {
+	           i++;
+	           if(i>=n)
+	           {
+	               i=0;
+	           }
+	        }
+	        else
+	        {
+	            i=i-1;
+	            j=t+1;
+	        }  
+	    }
+	}
+	for(i=0;i<n;i++)
+	{
+	    for(j=0;j<n;j++)
+	    {
+	        cout<<arr[i][j]<<"\t";
+	    }
+	    cout<<"\n";
+	}
+	return 0;
+}
