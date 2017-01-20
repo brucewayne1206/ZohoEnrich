a457f32b4e28f99d068c8abc105eafc43c55d186
brucewayne1206 <hari.6896@gmail.com>
2017-01-20 22:56:46 +0530

    Create subsetrecur.c

---
+++ subsetrecur.c
@@ -0,0 +1,29 @@
+# ZohoEnrich
+#include<stdio.h>
+#include<string.h>
+char arr[100];
+int subset(int n,int i,int j)
+{
+	int newsub=1<<j;
+	if(j<n)
+	{
+		if((i&newsub)!=0)
+		{
+			printf("%c ",arr[j]);
+		}
+		subset(n,i,j+1);
+	}
+}
+int main()
+{
+	scanf("%s",arr);
+	int size=strlen(arr);
+	int sub=1<<size;
+	for(int i=0;i<sub;i++)
+	{
+		printf("{ ");
+		subset(size,i,0);
+		printf("}");
+	}
+	return 0;
+}
