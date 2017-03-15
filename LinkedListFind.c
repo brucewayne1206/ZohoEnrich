b8ac217a7cef50e7217aacd00a55992592f5137e
brucewayne1206 <hari.6896@gmail.com>
2017-03-15 20:54:10 +0530

    Create LinkedListFind.c

---
+++ LinkedListFind.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+struct node 
+{
+   int data;
+   struct node *next;
+};
+
+struct node *h = NULL;
+struct node *p = NULL;
+
+void insert(int n) 
+{
+   p->data = n;
+   p->next = h;
+   h = p;
+}
+
+int find(int n)
+{
+    p=h;
+    int cnt;
+    while(p->next==NULL)
+    {
+        cnt++;
+        if(p->data==n)
+        {
+            return cnt;
+        }
+        p=p->next;
+    }
+    return 0;
+}
+int main()
+{
+    int ch,n,ind;
+	printf("\n1. Create node\n2. Print index\n Enter Choice: ");
+    do
+    {
+        scanf("%d",&ch);
+        if(ch==1)
+        {
+            int d;
+            scanf("%d",&d);
+            insert(d);
+        }
+        else if(ch==2)
+        {
+            printf("Enter the number to e searched: ");
+            scanf("%d",&n);
+            ind = find(n);
+            if(ind == 0)
+            {
+                printf("\nData not Found");
+            }
+            else
+            {
+                printf("\nIndex : %d",ind);
+            }
+        }
+        
+    }while(ch==1||ch==2);
+}
