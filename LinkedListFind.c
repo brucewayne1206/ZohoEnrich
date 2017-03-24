2cde593320bd8a88e0331d590e12fe7f729131d6
brucewayne1206 <hari.6896@gmail.com>
2017-03-24 11:30:34 +0530

    Update LinkedListFind.c

--- LinkedListFind.c
+++ LinkedListFind.c
@@ -1,38 +1,59 @@
 #include<stdio.h>
-struct list 
+#include<stdlib.h>
+struct node 
 {
    int data;
-   struct list *next;
-}node;
+   struct node *next;
+};
 
-node *h = NULL;
-node *p = NULL;
+struct node *h =(struct node*)malloc(sizeof(struct node*));
 
 void insert(int n) 
 {
+    //if(h==NULL){
+    //    h->data=n;
+   //     h->next=NULL;
+   // }
+    //else{
+    struct node *p =(struct node*)malloc(sizeof(struct node*));
+    
    p->data = n;
    p->next = h;
-   h = p;
+   
+    //return p;
+    h=p;
+    //}
+    
+    //if(h->next==NULL)
+    //printf("\nHI");
+    //else
+    //printf("%d",h->next->data);
+    //printf("%d",h->next->data);
 }
 
 int find(int n)
 {
+    struct node *p =(struct node*)malloc(sizeof(struct node*));
     p=h;
-    int cnt;
-    while(p->next==NULL)
+    int cnt=-1;
+    //printf("%d",p->data);
+    while(p!=NULL)
     {
         cnt++;
+        //printf("%d",p->data);
         if(p->data==n)
         {
             return cnt;
         }
         p=p->next;
     }
-    return 0;
+    
+    return cnt;
 }
 int main()
 {
     int ch,n,ind;
+    h=NULL;
 	printf("\n1. Create node\n2. Print index\n Enter Choice: ");
     do
     {
@@ -48,7 +69,7 @@ int main()
             printf("Enter the number to e searched: ");
             scanf("%d",&n);
             ind = find(n);
-            if(ind == 0)
+            if(ind == -1)
             {
                 printf("\nData not Found");
             }
